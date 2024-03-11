a75a51c73d172d1004d48b1c013451cae50f6e9e
DevelDanil <zherebtsov.da@mail.ru>
2024-03-11 13:58:42 +0700

    Добавьте файлы проекта.

---
+++ GameTamagotchi/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamagotchiPlay
+{
+    public class HomePet
+    {
+        static void Main()
+        {
+            bool playAgain = true;
+            while (playAgain)
+            {
+                Console.WriteLine("                                ¶¶¶¶¶¶¶¶¶\r\n                              ¶¶¶  ¶¶¶¶¶¶\r\n                            ¶¶    ¶¶              ¶¶¶¶¶¶\r\n                            ¶   ¶¶             ¶¶¶¶ ¶¶¶¶¶¶¶¶¶¶\r\n                           ¶¶   ¶¶¶¶¶¶¶¶      ¶¶   ¶¶       ¶¶¶¶¶¶\r\n                           ¶¶ ¶¶¶¶     ¶¶¶¶¶¶¶       ¶         ¶¶_¶¶\r\n                            ¶¶¶            ¶¶       ¶¶         ¶  ¶\r\n                           ¶¶             ¶¶         ¶         ¶¶ ¶¶\r\n                          ¶¶              ¶¶         ¶          ¶ ¶¶\r\n                          ¶                 ¶¶¶¶    ¶¶ ¶¶    ¶¶¶¶  ¶\r\n                          ¶                ¶¶¶¶¶¶¶¶¶  ¶O¶  ¶O¶ ¶ ¶¶\r\n                          ¶                ¶¶¶¶   ¶¶         ¶¶  ¶¶\r\n                          ¶¶                ¶ ¶¶            ¶ ¶¶\r\n                          ¶                 ¶¶ ¶¶¶¶¶¶ ¶     ¶¶¶¶\r\n                         ¶        ¶          ¶¶¶¶¶ ¶¶¶¶     ¶¶¶\r\n                         ¶      ¶¶¶¶¶          ¶¶¶¶¶¶¶¶¶¶¶¶¶¶\r\n                         ¶     ¶¶ ¶ ¶¶¶             ¶  ¶¶¶   ¶¶\r\n                         ¶     ¶  ¶¶  ¶¶¶      ¶¶¶ ¶   ¶¶¶¶ ¶¶¶¶¶\r\n                         ¶       ¶  ¶¶   ¶       ¶ ¶¶¶    ¶ ¶¶¶¶¶¶¶¶\r\n                         ¶¶  ¶¶¶¶   ¶¶¶¶¶¶     ¶¶ ¶ ¶    ¶¶¶¶¶¶¶¶¶¶\r\n                          ¶¶¶¶¶¶¶       ¶¶¶       ¶¶¶  ¶     ¶¶¶¶¶¶¶¶\r\n                                          ¶     ¶¶  ¶¶¶       ¶  ¶¶\r\n                                          ¶¶     ¶¶¶ ¶¶¶         ¶¶\r\n                                           ¶¶     ¶¶ ¶¶¶¶¶     ¶¶\r\n                                           ¶¶¶¶ ¶¶¶     ¶¶¶¶¶¶¶\r\n                                              ¶¶¶¶¶\n");
+                Console.WriteLine("                         Доброго времени суток! Добро пожаловать в игру тамагочи.\n");
+                string NamePet;
+                do
+                {
+                    Console.WriteLine("Для старта игры введите ведите имя питомца: ");
+                    NamePet = Console.ReadLine();
+                    if (NamePet == "")
+                    {
+                        Console.WriteLine("Вы не ввели имя питомца");
+                    }
+                } while (NamePet == "");
+                Console.WriteLine($"{NamePet} изначальное имеет уровень здоровья: 10 \nуровень голода: 0\nуровень усталости: 0\n");
+                Tamagotchi pet = new Tamagotchi(10, 0, 0);
+                string userInput;
+                while (pet.Health > 0)
+                {
+                    Console.WriteLine($"\nВыберите действие для {NamePet}:");
+                    Console.WriteLine("1.Покормить");
+                    Console.WriteLine("2.Поиграть");
+                    Console.WriteLine("3.Укачать(сон)");
+                    userInput = Console.ReadLine();
+                    switch (userInput)
+                    {
+                        case "1":
+                            pet.Feed();
+                            break;
+
+                        case "2":
+                            pet.Play();
+                            break;
+
+                        case "3":
+                            pet.Rock();
+                            break;
+                        default:
+                            Console.WriteLine("\nНекорректный ввод, пожалуйста выберите действие из списка");
+                            break;
+                    }
+                    Console.WriteLine($"{NamePet} имеет уровень здоровья " + pet.Health);
+                    Console.WriteLine("Уровень голода питомца " + pet.Hunger);
+                    Console.WriteLine("Уровень усталости питомца " + pet.Fatigue);
+                }
+                Console.WriteLine("К сожалению, Ваш питомец погиб.");
+                string playAgainInput;
+                do
+                {
+                    Console.WriteLine("Хотите начать новую игру? (да/нет)");
+                    playAgainInput = Console.ReadLine().ToLower();
+                    if (playAgainInput != "да" && playAgainInput != "нет")
+                    {
+                        Console.WriteLine("Пожалуйста, введите 'да' или 'нет'");
+                    }
+                } while (playAgainInput != "да" && playAgainInput != "нет");
+
+                playAgain = (playAgainInput == "да");
+            }
+        }
+    }
+}
---
+++ GameTamagotchi/Tamagotchi.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamagotchiPlay
+{
+    public class Tamagotchi
+    {
+        public int Health { get; set; }
+        public int Hunger { get; set; }
+        public int Fatigue { get; set; }
+        public Tamagotchi(int health, int hunger, int fatigue)
+        {
+            Health = health;
+            Hunger = hunger;
+            Fatigue = fatigue;
+        }
+        public void Feed()
+        {
+            Hunger -= 1;
+            if (Hunger <= 0)
+            {
+                if (Health <= 0)
+                {
+                    Console.WriteLine("К сожалению, Вы проиграли.");
+                }
+                else
+                {
+                    Health -= 1;
+                }
+                Hunger = 0;
+            }
+        }
+        public void Play()
+        {
+            if (Fatigue >= 10)
+            {
+                Health -= 1;
+                if (Hunger < 10)
+                {
+                    Hunger += 1;
+                }
+            }
+            else
+            {
+                Fatigue += 1;
+            }
+        }
+        public void Rock()
+        {
+            if (Health >= 10)
+            {
+                Health = 10;
+            }
+            else
+            {
+                Health += 1;
+            }
+            Fatigue = 0;
+            if (Hunger >= 10)
+            {
+                Health -= 1;
+                Hunger = 10;
+            }
+            else
+            {
+                Hunger += 1;
+            }
+
+        }
+    }
+}
